b3ea70211734360d76d882aaec29d4c23cdb3cef
Elena Ryzhkina <silena003@gmail.com>
2022-02-15 20:28:37 +0300

    Initial commit

---
+++ src/test/java/ru/netology/manager/ProductManagerTest.java
@@ -0,0 +1,17 @@
+package ru.netology.manager;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProductManagerTest {
+
+    @Test
+    void add() {
+
+    }
+
+    @Test
+    void searcyBy() {
+    }
+}
